aa8fd58bb055bffc8b543f659707e5bb045a463b
krystek77 <krystian.wrona77@gmail.com>
2022-02-26 10:16:37 +0100

    Remove scroll buttons from layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,10 @@
 import styles from './Layout.module.css';
 import Topbar from '../../containers/Topbar/Topbar';
-import ScrollButton from '../ScrollButton/ScrollButton';
 
 function Layout({ children }) {
   return (
     <div className={styles.layout}>
       <Topbar/>
-      <ScrollButton down />
-      <ScrollButton up />
       <main className={styles.layout__content}>{children}</main>
       <h1>Footer</h1>
     </div>
