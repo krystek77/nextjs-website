0a37fa96a5e47036e10103e0d31ef6c3d2a2a1af
krystek77 <krystian.wrona77@gmail.com>
2022-03-01 12:21:02 +0100

    Create hygiene barrier section for home page

---
+++ containers/HygieneBarrier/HygieneBarrier.js
@@ -0,0 +1,24 @@
+import styles from './HygieneBarrier.module.css';
+import Title from '../../components/Title/Title';
+import LinkButton from '../../components/LinkButton/LinkButton';
+import Image from 'next/image';
+
+function HygieneBarrier() {
+  return (
+    <section className={styles.hygieneBarrier}>
+      <Image
+        src="/assets/images/bg_hygiene_barrier.png"
+        alt="bariera higieny"
+        layout="fill"
+      />
+      <Title
+        content="Skuteczna ochrona przed przenoszeniem zakażeń i bakteriami"
+        variant="h1"
+        classes="title_light_500 title_center title_maxWidth_960"
+      />
+      <LinkButton label="Idea pralni z barierą higieny" to="/bariera-higieny" />
+    </section>
+  );
+}
+
+export default HygieneBarrier;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import Hero from "../containers/Hero/Hero";
-import ModernLaundryEquipment from "../containers/ModernLaundryEquipment/ModernLaundryEquipment";
+import React from 'react';
+import Hero from '../containers/Hero/Hero';
+import ModernLaundryEquipment from '../containers/ModernLaundryEquipment/ModernLaundryEquipment';
+import HygieneBarrier from '../containers/HygieneBarrier/HygieneBarrier';
 
 export default function Home() {
   return (
     <React.Fragment>
       <Hero />
-      <ModernLaundryEquipment/>
+      <ModernLaundryEquipment />
+      <HygieneBarrier />
     </React.Fragment>
   );
 }
