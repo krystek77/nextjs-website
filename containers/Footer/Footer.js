be7201e144c9c1138a41304b50c30262402f6fdb
krystek77 <krystian.wrona77@gmail.com>
2022-03-03 11:26:16 +0100

    Start creagting footer

--- containers/Footer/Footer.js
+++ containers/Footer/Footer.js
@@ -1,6 +1,14 @@
 import styles from './Footer.module.css';
 
-function Footer(){
-    return <footer className={styles.footer}>Footer</footer>
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__inner}>
+        <p className={styles.copyright}>
+          Copyright 2022 by Krystian Wrona. Wszystkie prawa zastrzeżone.
+        </p>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
