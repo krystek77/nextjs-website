afa1808acb3171820cbb5d8537afdfbdd89d5d37
krystek77 <krystian.wrona77@gmail.com>
2022-03-11 08:02:07 +0100

    Move data for pages to constants folder

--- pages/urzadzenia-pralnia/pralnicowirowki/FX/[model].js
+++ pages/urzadzenia-pralnia/pralnicowirowki/FX/[model].js
@@ -17,95 +17,6 @@ import CascadeDrum from '../../../../containers/CascadeDrum/CascadeDrum';
 import FXDesign from '../../../../containers/FXDesign/FXDesign';
 import Friendly from '../../../../containers/Friendly/Friendly';
 
-const parameters = [
-  { name: 'załadunek (moduł załadunku 1:10)', value: '10.5', unit: 'kg' },
-  {
-    name: 'załadunek maksymalny (moduł załadunku 1:9)',
-    value: '12',
-    unit: 'kg',
-  },
-  { name: 'pojemność bębna', value: '105', unit: 'litrów' },
-  { name: 'średnica bębna', value: '620', unit: 'mm' },
-  { name: 'obrtoty prania', value: '49', unit: 'obr./min.' },
-  { name: 'obrtoty wirowania', value: '1075', unit: 'obr./min.' },
-  { name: 'G-faktor', value: '400', unit: '-' },
-  { name: 'moc podgrzewu elektrycznego', value: '6/9/12', unit: 'kW' },
-  {
-    name: 'podłączenie pary (dla podgrzewu parowego)',
-    value: '1/2',
-    unit: 'cal',
-  },
-  {
-    name: 'ciśnienie pary (dla podgrzewu parowego)',
-    value: '1 - 8',
-    unit: 'bar',
-  },
-  { name: 'podłączenie wody', value: '3/4', unit: 'cal' },
-  { name: 'ciśnienie wody', value: '1 - 8', unit: 'bar' },
-  { name: 'zasilanie (standard)', value: '3x380 - 415/50Hz/3', unit: '-' },
-  { name: 'wymiary (w. x sz. x gł.)', value: '1225x795x795', unit: 'mm' },
-  { name: 'masa netto', value: '210', unit: 'kg' },
-  { name: "masa brutto'", value: '235', unit: 'kg' },
-];
-const features = [
-  'wolnostojąca wysokoobrotowa',
-  'nierdzewny panel górny',
-  'przedni i boczne panele ocynkowane lakierowane w aktrakcyjnym kolorze stali INOX',
-  'nierdzewny bęben wewnętrzny i zewnętrzny',
-  'XControl - łatwy w użyciu sterownik mikroprocesorowy',
-  'opatentowany pojemnik na detergenty',
-  'duża średnica zaworu spustowego - 76 mm',
-  'łatwy dostęp do wszystkich istotnych podzespołów',
-  'szeroko otwierane drzwi dla łatwego załadunku i wyładunku',
-  'moduł podłączenia do 8 sygnałów pomp płynnych środków piorących',
-  'napęd z przemiennikiem częstotliwości',
-];
-const leaflets = [
-  { label: 'karta informacyjna', fileName: 'FX6580105135 KI' },
-  {
-    label: 'instrukcja instalacji pralnicowirówki',
-    fileName: 'FX105 instalacja',
-  },
-];
-
-const controls = [
-  {
-    name: 'XControl FLEX +',
-    image: 'XControl FLEX Plus',
-    list: [
-      '7 calowy ekran dotykoway',
-      'możliwość wyświetlania komunikatów w 34 językach',
-      'wbudowane wifi',
-      'zdalne programowanie i aktualizacje',
-      'w pełni programowalny',
-      '20 fabrycznych programów prania spełniających wszystkie podstawowe potrzeby',
-      'do 99 programów użytkownika',
-    ],
-  },
-  {
-    name: 'XControl +',
-    image: 'XControl Plus',
-    list: [
-      'do 99 programów prania',
-      'w pełni programowalny',
-      'wyświetlacz graficzny',
-      'obsługa w 21 językach, w tym polskim',
-      'port USB',
-    ],
-  },
-  {
-    name: 'XControl',
-    image: 'XControl',
-    list: [
-      '15 programów prania',
-      'w pełni programowalny',
-      'wyświetlacz graficzny',
-      'obsługa w 25 językach, w tym w polskim',
-      'port USB',
-    ],
-  },
-];
-
 function FX(props) {
   const {
     model,
@@ -179,126 +90,18 @@ export async function getStaticPaths(context) {
   };
 }
 
+import { data } from '../../../../constants/data';
+
 export async function getStaticProps(context) {
   //fetch data for a single washer extractors
   // console.log(context);
   // console.log(context.params.model);
+  const washerExtractor = data.find(
+    (item) => item.model === context.params.model
+  );
+
   return {
-    props: {
-      model: 'FX-65',
-      title: 'Pralnicowirówka wysokoobrotowa, typ FX-65',
-      category: 'Pralnicowirówki wysokoobrotowe',
-      line: 'Linia FX',
-      available_models: [
-        { title: 'FX-65', subtitle: 'załadunek 6.5 - 7 kg' },
-        { title: 'FX-80', subtitle: 'załadunek 8 - 9 kg' },
-        { title: 'FX-105', subtitle: 'załadunek 10.5 - 12 kg' },
-        { title: 'FX-135', subtitle: 'załadunek 13.5 - 15 kg' },
-        { title: 'FX-180', subtitle: 'załadunek 18 - 20 kg' },
-        { title: 'FX-240', subtitle: 'załadunek 24 - 27 kg' },
-        { title: 'FX-280', subtitle: 'załadunek 28 -32 kg' },
-      ],
-      slider: [
-        'fx105_01',
-        'fx105_02',
-        'fx105_03',
-        'fx105_04',
-        'fx105_05',
-        'fx105_06',
-      ],
-      isSliderVertical: false,
-      features: [
-        'wolnostojąca wysokoobrotowa',
-        'nierdzewny panel górny',
-        'przedni i boczne panele ocynkowane lakierowane w aktrakcyjnym kolorze stali INOX',
-        'nierdzewny bęben wewnętrzny i zewnętrzny',
-        'XControl - łatwy w użyciu sterownik mikroprocesorowy',
-        'opatentowany pojemnik na detergenty',
-        'duża średnica zaworu spustowego - 76 mm',
-        'łatwy dostęp do wszystkich istotnych podzespołów',
-        'szeroko otwierane drzwi dla łatwego załadunku i wyładunku',
-        'moduł podłączenia do 8 sygnałów pomp płynnych środków piorących',
-        'napęd z przemiennikiem częstotliwości',
-      ],
-      parameters: [
-        { name: 'załadunek (moduł załadunku 1:10)', value: '10.5', unit: 'kg' },
-        {
-          name: 'załadunek maksymalny (moduł załadunku 1:9)',
-          value: '12',
-          unit: 'kg',
-        },
-        { name: 'pojemność bębna', value: '105', unit: 'litrów' },
-        { name: 'średnica bębna', value: '620', unit: 'mm' },
-        { name: 'obrtoty prania', value: '49', unit: 'obr./min.' },
-        { name: 'obrtoty wirowania', value: '1075', unit: 'obr./min.' },
-        { name: 'G-faktor', value: '400', unit: '-' },
-        { name: 'moc podgrzewu elektrycznego', value: '6/9/12', unit: 'kW' },
-        {
-          name: 'podłączenie pary (dla podgrzewu parowego)',
-          value: '1/2',
-          unit: 'cal',
-        },
-        {
-          name: 'ciśnienie pary (dla podgrzewu parowego)',
-          value: '1 - 8',
-          unit: 'bar',
-        },
-        { name: 'podłączenie wody', value: '3/4', unit: 'cal' },
-        { name: 'ciśnienie wody', value: '1 - 8', unit: 'bar' },
-        {
-          name: 'zasilanie (standard)',
-          value: '3x380 - 415/50Hz/3',
-          unit: '-',
-        },
-        { name: 'wymiary (w. x sz. x gł.)', value: '1225x795x795', unit: 'mm' },
-        { name: 'masa netto', value: '210', unit: 'kg' },
-        { name: "masa brutto'", value: '235', unit: 'kg' },
-      ],
-      controls: [
-        {
-          name: 'XControl FLEX +',
-          image: 'XControl FLEX Plus',
-          list: [
-            '7 calowy ekran dotykoway',
-            'możliwość wyświetlania komunikatów w 34 językach',
-            'wbudowane wifi',
-            'zdalne programowanie i aktualizacje',
-            'w pełni programowalny',
-            '20 fabrycznych programów prania spełniających wszystkie podstawowe potrzeby',
-            'do 99 programów użytkownika',
-          ],
-        },
-        {
-          name: 'XControl +',
-          image: 'XControl Plus',
-          list: [
-            'do 99 programów prania',
-            'w pełni programowalny',
-            'wyświetlacz graficzny',
-            'obsługa w 21 językach, w tym polskim',
-            'port USB',
-          ],
-        },
-        {
-          name: 'XControl',
-          image: 'XControl',
-          list: [
-            '15 programów prania',
-            'w pełni programowalny',
-            'wyświetlacz graficzny',
-            'obsługa w 25 językach, w tym w polskim',
-            'port USB',
-          ],
-        },
-      ],
-      leaflets: [
-        { label: 'karta informacyjna', fileName: 'FX6580105135 KI' },
-        {
-          label: 'instrukcja instalacji pralnicowirówki',
-          fileName: 'FX105 instalacja',
-        },
-      ],
-    },
+    props: washerExtractor,
   };
 }
 export default FX;
