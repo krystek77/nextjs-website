e9bf9caddfd1c291c620a4fd8eac0a3cc455f22f
krystek77 <krystian.wrona77@gmail.com>
2022-03-13 11:44:08 +0100

    Start building spare parts page

--- pages/czesci-zamienne-pralki-przemyslowe/index.js
+++ pages/czesci-zamienne-pralki-przemyslowe/index.js
@@ -4,10 +4,17 @@
 import React from "react";
 import HeadMetaTags from "../../components/HeadMetaTags/HeadMetaTags";
 import SiteInProgess from "../../containers/SiteInProgress/SiteInProgress";
+import Banner from "../../components/Banner/Banner";
+import Title from "../../components/Title/Title";
+import Subtitle from "../../components/Subtitle/Subtitle";
 function SpareParts() {
   return (
     <React.Fragment>
       <HeadMetaTags />
+      <Banner classes=''>
+        <Subtitle content='Do przemysłowych urządzeń pralniczych' classes="subtitle__ball_dark" />
+        <Title content='Popularne części eksploatacyjne i zamienne' classes='title_bg_white_dark title_maxWidth_960' />
+      </Banner>
       <SiteInProgess page='CZĘŚCI ZAMIENNE' />
     </React.Fragment>
   );
