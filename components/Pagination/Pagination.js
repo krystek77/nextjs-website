3f25e74df54e989ace36b9bae4799611934a3f70
krystek77 <krystian.wrona77@gmail.com>
2022-03-16 19:25:39 +0100

    Limit the number of pagination buttons

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -4,6 +4,7 @@ import styles from "./Pagination.module.css";
 import router from "next/router";
 
 function Pagination({ pageNumber, href, page }) {
+  const COUNT_LINKS = 2;
   const prev = () => {
     if (page > 1) {
       router.push(`${href}/${page - 1}`);
@@ -20,11 +21,11 @@ function Pagination({ pageNumber, href, page }) {
         <Image src='/assets/icons/arrow_left_thin.svg' alt='poprzednia strona' width='16' height='14' />
       </button>
       {Array.from({ length: pageNumber }).map((_, index) => {
-        return (
+        return index  <= COUNT_LINKS - 1 ? (
           <Link key={index} href={`${href}/${index + 1}`} scroll={false}>
             <a className={styles.pagination__link}>{index + 1}</a>
           </Link>
-        );
+        ) : null;
       })}
       <button className={`${styles.pagination__button} ${page === pageNumber && styles.pagination__button_disabled}`} type='button' onClick={next}>
         <Image src='/assets/icons/arrow_right_thin.svg' alt='następna strona' width='16' height='14' />
