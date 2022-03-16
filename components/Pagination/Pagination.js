2b0f3167b1a097b9f9e37fafacaff4cd7a950b83
krystek77 <krystian.wrona77@gmail.com>
2022-03-16 19:34:10 +0100

    Replace buttons with links and prevent page scrolling

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -5,31 +5,41 @@ import router from "next/router";
 
 function Pagination({ pageNumber, href, page }) {
   const COUNT_LINKS = 2;
-  const prev = () => {
-    if (page > 1) {
-      router.push(`${href}/${page - 1}`);
-    }
-  };
-  const next = () => {
-    if (page < pageNumber) {
-      router.push(`${href}/${page + 1}`);
-    }
-  };
+  // const prev = () => {
+  //   if (page > 1) {
+  //     router.push(`${href}/${page - 1}`);
+  //   }
+  // };
+  // const next = () => {
+  //   if (page < pageNumber) {
+  //     router.push(`${href}/${page + 1}`);
+  //   }
+  // };
   return (
     <section className={styles.pagination}>
-      <button className={`${styles.pagination__button} ${page === 1 && styles.pagination__button_disabled}`} type='button' onClick={prev}>
+      <Link href={`${href}/${page - 1}`} type='button' scroll={false}>
+        <a className={`${styles.pagination__button} ${page === 1 && styles.pagination__button_disabled}`}>
+          <Image src='/assets/icons/arrow_left_thin.svg' alt='poprzednia strona' width='16' height='14' />
+        </a>
+      </Link>
+      {/* <button className={`${styles.pagination__button} ${page === 1 && styles.pagination__button_disabled}`} type='button' onClick={prev}>
         <Image src='/assets/icons/arrow_left_thin.svg' alt='poprzednia strona' width='16' height='14' />
-      </button>
+      </button> */}
       {Array.from({ length: pageNumber }).map((_, index) => {
-        return index  <= COUNT_LINKS - 1 ? (
+        return index <= COUNT_LINKS - 1 ? (
           <Link key={index} href={`${href}/${index + 1}`} scroll={false}>
             <a className={styles.pagination__link}>{index + 1}</a>
           </Link>
         ) : null;
       })}
-      <button className={`${styles.pagination__button} ${page === pageNumber && styles.pagination__button_disabled}`} type='button' onClick={next}>
+      <Link href={`${href}/${page + 1}`} type='button' scroll={false}>
+        <a className={`${styles.pagination__button} ${page === pageNumber && styles.pagination__button_disabled}`}>
+          <Image src='/assets/icons/arrow_right_thin.svg' alt='następna strona' width='16' height='14' />
+        </a>
+      </Link>
+      {/* <button className={`${styles.pagination__button} ${page === pageNumber && styles.pagination__button_disabled}`} type='button' onClick={next}>
         <Image src='/assets/icons/arrow_right_thin.svg' alt='następna strona' width='16' height='14' />
-      </button>
+      </button> */}
     </section>
   );
 }
