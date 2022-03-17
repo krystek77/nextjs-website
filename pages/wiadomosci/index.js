e3d29d06f6d42f65dca1e08591e3e72e7cdf6ec7
krystek77 <krystian.wrona77@gmail.com>
2022-03-17 13:53:48 +0100

    Set background for news page banner

--- pages/wiadomosci/index.js
+++ pages/wiadomosci/index.js
@@ -2,21 +2,27 @@
  * http://localhost:3000/wiadomosci
  */
 
-import React from "react";
-import Banner from "../../components/Banner/Banner";
-import Title from "../../components/Title/Title";
-import HeadMetaTags from "../../components/HeadMetaTags/HeadMetaTags";
-import SiteInProgress from "../../containers/SiteInProgress/SiteInProgress";
-import styles from "./index.module.css";
+import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Title from '../../components/Title/Title';
+import HeadMetaTags from '../../components/HeadMetaTags/HeadMetaTags';
+import SiteInProgress from '../../containers/SiteInProgress/SiteInProgress';
+import styles from './index.module.css';
+import Subtitle from '../../components/Subtitle/Subtitle';
 
 function Posts() {
   return (
     <React.Fragment>
       <HeadMetaTags />
-      <Banner>
-        <Title variant='h1' content='Aktualności z Pralma' classes='title_maxWidth_960 title_light_500' />
+      <Banner classes="banner__news">
+        <Subtitle content="Bądź na bieżąco" />
+        <Title
+          variant="h1"
+          content="Wiadomości ze świata Pralma"
+          classes="title_bg_white_red title_maxWidth_640 title_light_500"
+        />
       </Banner>
-      <SiteInProgress page='WIADOMOŚCI' />
+      <SiteInProgress page="WIADOMOŚCI" />
     </React.Fragment>
   );
 }
