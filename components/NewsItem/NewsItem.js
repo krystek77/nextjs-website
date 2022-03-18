509e4bb842acaf9d9e5c0901772473014fd5dd79
krystek77 <krystian.wrona77@gmail.com>
2022-03-18 07:51:19 +0100

    Trim post description to specified length maximum

--- components/NewsItem/NewsItem.js
+++ components/NewsItem/NewsItem.js
@@ -1,27 +1,37 @@
-import Image from "next/image";
-import Title from "../Title/Title";
-import Description from "../Description/Description";
-import LinkButton from "../LinkButton/LinkButton";
+import Image from 'next/image';
+import Title from '../Title/Title';
+import Description from '../Description/Description';
+import LinkButton from '../LinkButton/LinkButton';
+import { trimmedStringToWord } from '../../lib';
 
-import styles from "./NewsItem.module.css";
+import styles from './NewsItem.module.css';
 
 function NewsItem({ id, title, date, image, info }) {
   return (
-    <div className={styles.newsItem}>
-      <div className={styles.newsItem__image}>
+    <article className={styles.newsItem}>
+      <aside className={styles.newsItem__image}>
         {/* <Image src={image} alt={title} width='426' height='320' /> */}
-        <img src={image} alt={title} width='360' height='270' />
-      </div>
-      <div className={styles.newsItem__content}>
+        <img src={image} alt={title} width="360" height="270" />
+      </aside>
+      <header className={styles.newsItem__header}>
         <span className={styles.newsItem__date}>{date}</span>
-        <Title content={title} variant='h2' classes='title_display_h4' />
-        <Description content={info} classes='description_18 description_mb_2' />
+        <Title
+          content={title}
+          variant="h2"
+          classes="title_display_h5 title_uppercase title_weight_small"
+        />
+      </header>
+      <div className={styles.newsItem__content}>
+        <Description
+          content={trimmedStringToWord(info)}
+          classes="description_18"
+        />
       </div>
       <LinkButton
-        label='Przeczytaj'
+        label="Przeczytaj"
         to={`/wiadomosci/${encodeURIComponent(id)}`} //for slug
       />
-    </div>
+    </article>
   );
 }
 export default NewsItem;
