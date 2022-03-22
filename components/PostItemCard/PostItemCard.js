310a469584dcbd0ce67d1f53de526f6f287cb9f9
krystek77 <krystian.wrona77@gmail.com>
2022-03-22 14:40:41 +0100

    Change styles of post item card component

--- components/PostItemCard/PostItemCard.js
+++ components/PostItemCard/PostItemCard.js
@@ -1,31 +1,44 @@
-import Title from "../Title/Title";
-import Description from "../Description/Description";
-import LinkButton from "../LinkButton/LinkButton";
-import moment from "moment";
-import "moment/locale/pl";
-moment.locale("pl");
+import Title from '../Title/Title';
+import Description from '../Description/Description';
+import LinkButton from '../LinkButton/LinkButton';
+import moment from 'moment';
+import 'moment/locale/pl';
+moment.locale('pl');
 
-import { trimmedStringToWord, classesNames } from "../../lib";
-import styles from "./PostItemCard.module.css";
+import { trimmedStringToWord, classesNames } from '../../lib';
+import styles from './PostItemCard.module.css';
 
-function PostItemCard({ id, title, date, image, info, classes = "", tags }) {
-  const classesString = classesNames(classes, "postItemCard")(styles);
+function PostItemCard({ id, title, date, image, info, classes = '', tags }) {
+  const classesString = classesNames(classes, 'postItemCard')(styles);
   return (
     <article className={classesString}>
       <aside className={styles.postItemCard__image}>
-        <img src={image} alt={title} width='360' height='270' />
+        <img src={image} alt={title} width="360" height="270" />
       </aside>
       <header className={styles.postItemCard__header}>
-        <span className={styles.postItemCard__date}>{moment(date, "YYYY-MM-DD").fromNow()}</span>
-        <Title content={title} variant='h2' classes='title_display_h5 title_uppercase title_weight_small' />
+        <span className={styles.postItemCard__date}>
+          {moment(date, 'YYYY-MM-DD').fromNow()}
+        </span>
+        <Title
+          content={title}
+          variant="h2"
+          classes="title_display_h5 title_uppercase title_weight_small"
+        />
       </header>
       <div className={styles.postItemCard__content}>
-        <Description content={trimmedStringToWord(info)} classes='description_18' />
+        <Description
+          content={trimmedStringToWord(info)}
+          classes="description_18"
+        />
         <p className={styles.postItemCard__tags}>
           Kategoria: <span className={styles.postItemCard__tag}>{tags}</span>
         </p>
       </div>
-      <LinkButton label='Przeczytaj' to={`/wiadomosci/${encodeURIComponent(id)}`} />
+      <LinkButton
+        classes="linkButton_center"
+        label="Przeczytaj"
+        to={`/wiadomosci/${encodeURIComponent(id)}`}
+      />
     </article>
   );
 }
