289e1256ff35db7605a9289fcc9a3d8c5b5f5f11
krystek77 <krystian.wrona77@gmail.com>
2022-03-23 07:02:05 +0100

    Create todo list to remember what is to do

---
+++ todo.js
@@ -0,0 +1,19 @@
+/**
+ * TODO:
+ * - create customers section
+ * - create contact form
+ * - create contact page
+ * - create testimonials section
+ * - add content to other pages
+ * - customieze product data structure
+ * - strore the spare parts in MongoDB database
+ * - download the euro exchange rate by NBP API to calculate the spare part price
+ * - customize the pagination appearance for more subpages
+ * - display laundry photos in modal
+ * - add logic to navigation for small screens (maybe custom hook from accordion)
+ * - add login logic
+ * - add media query to make the website responsive
+ * - website optimization - React, accessibilty, SEO
+ * - testing
+ * - deploy website
+ */
