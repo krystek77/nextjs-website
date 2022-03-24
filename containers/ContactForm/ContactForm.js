c2f4b94a454ae3d8df08f9e95faac01b3575ba4b
krystek77 <krystian.wrona77@gmail.com>
2022-03-24 07:27:24 +0100

    Add short description to contact form

--- containers/ContactForm/ContactForm.js
+++ containers/ContactForm/ContactForm.js
@@ -1,21 +1,22 @@
-import React from "react";
-import Button from "../../components/Button/Button";
-import Input from "../../components/Input/Input";
-import Title from "../../components/Title/Title";
-import Subtitle from "../../components/Subtitle/Subtitle";
-import styles from "./ContactForm.module.css";
+import React from 'react';
+import Button from '../../components/Button/Button';
+import Input from '../../components/Input/Input';
+import Title from '../../components/Title/Title';
+import Subtitle from '../../components/Subtitle/Subtitle';
+import Description from '../../components/Description/Description';
+import styles from './ContactForm.module.css';
 
 function ContactForm() {
   const [formData, setFormData] = React.useState({
-    reason: "",
-    market: "",
-    name: "",
-    lastName: "",
-    phone: "",
-    city: "",
-    company: "",
-    email: "",
-    message: "",
+    reason: '',
+    market: '',
+    name: '',
+    lastName: '',
+    phone: '',
+    city: '',
+    company: '',
+    email: '',
+    message: '',
   });
   const [isNewsletter, setIsNewsletter] = React.useState(true);
 
@@ -31,15 +32,15 @@ function ContactForm() {
   const handleNewsletter = (e) => setIsNewsletter(!isNewsletter);
   const clearForm = () => {
     setFormData({
-      reason: "",
-      market: "",
-      name: "",
-      lastName: "",
-      phone: "",
-      city: "",
-      company: "",
-      email: "",
-      message: "",
+      reason: '',
+      market: '',
+      name: '',
+      lastName: '',
+      phone: '',
+      city: '',
+      company: '',
+      email: '',
+      message: '',
     });
     setIsNewsletter(true);
   };
@@ -48,44 +49,123 @@ function ContactForm() {
     <section className={styles.contactFormSection}>
       <div className={styles.contactFormSection__container}>
         <header className={styles.contactFormSection__header}>
-          <Subtitle content='Napisz do nas' />
-          <Title content='Formularz kontaktowy' />
+          <Subtitle content="Napisz do nas" />
+          <Title content="Formularz kontaktowy" />
+          <Description classes="description_maxWidth_960 description_18">
+            Masz pytania dotyczące przemysłowych urządzeń pralniczych marki{' '}
+            <strong>Pralma</strong> lub <strong>Primus</strong>? Aby uzyskać
+            pomoc techniczną, rozwiązć problemy z eksploatcją, konserwacją
+            urządzeń, zapytać o ofertę na odpowiednie do Twoich potrzeb
+            wyposażenie pralnicze lub masz inne pytania dotyczące naszej
+            współpracy lub dokumnetów, wypełnij i prześlij poniższy formularz.
+          </Description>
         </header>
         <form className={styles.contactFormSection__form} onSubmit={handleForm}>
           <div className={styles.contactFormSection__reason}>
-            <Input classes='input_mb_1' type='text' fieldName='reason' handleInput={handleInput} value={formData.reason} placeholder='powód kontaktu' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="reason"
+              handleInput={handleInput}
+              value={formData.reason}
+              placeholder="powód kontaktu"
+            />
           </div>
           <div className={styles.contactFormSection__market}>
-            <Input classes='input_mb_1' type='text' fieldName='market' handleInput={handleInput} value={formData.market} placeholder='sektor pralniczy' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="market"
+              handleInput={handleInput}
+              value={formData.market}
+              placeholder="sektor pralniczy"
+            />
           </div>
           <div className={styles.contactFormSection__name}>
-            <Input classes='input_mb_1' type='text' fieldName='name' handleInput={handleInput} value={formData.name} placeholder='imię' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="name"
+              handleInput={handleInput}
+              value={formData.name}
+              placeholder="imię"
+            />
           </div>
           <div className={styles.contactFormSection__lastName}>
-            <Input classes='input_mb_1' type='text' fieldName='lastName' handleInput={handleInput} value={formData.lastName} placeholder='nazwisko' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="lastName"
+              handleInput={handleInput}
+              value={formData.lastName}
+              placeholder="nazwisko"
+            />
           </div>
           <div className={styles.contactFormSection__phone}>
-            <Input type='phone' fieldName='phone' handleInput={handleInput} value={formData.phone} placeholder='telefon' />
+            <Input
+              type="phone"
+              fieldName="phone"
+              handleInput={handleInput}
+              value={formData.phone}
+              placeholder="telefon"
+            />
           </div>
           <div className={styles.contactFormSection__city}>
-            <Input classes='input_mb_1' type='text' fieldName='city' handleInput={handleInput} value={formData.city} placeholder='miasto' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="city"
+              handleInput={handleInput}
+              value={formData.city}
+              placeholder="miasto"
+            />
           </div>
           <div className={styles.contactFormSection__company}>
-            <Input classes='input_mb_1' type='text' fieldName='company' handleInput={handleInput} value={formData.company} placeholder='firma / instytucja' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="company"
+              handleInput={handleInput}
+              value={formData.company}
+              placeholder="firma / instytucja"
+            />
           </div>
           <div className={styles.contactFormSection__email}>
-            <Input classes='input_mb_1' type='text' fieldName='email' handleInput={handleInput} value={formData.email} placeholder='email' />
+            <Input
+              classes="input_mb_1"
+              type="text"
+              fieldName="email"
+              handleInput={handleInput}
+              value={formData.email}
+              placeholder="email"
+            />
           </div>
           <div className={styles.contactFormSection__message}>
-            <textarea rows='10' name='message' onChange={handleInput} value={formData.message} placeholder='wpisz treść...' />
+            <textarea
+              rows="10"
+              name="message"
+              onChange={handleInput}
+              value={formData.message}
+              placeholder="wpisz treść..."
+            />
           </div>
           <div className={styles.contactFormSection__newsletter}>
-            <label className={styles.contactFormSection__label} htmlFor='newsletter'>
+            <label
+              className={styles.contactFormSection__label}
+              htmlFor="newsletter"
+            >
               zapisz się na newsletter
             </label>
-            <input className={styles.contactFormSection__input} name='newsletter' id='newsletter' type='checkbox' onChange={handleNewsletter} checked={isNewsletter} />
+            <input
+              className={styles.contactFormSection__input}
+              name="newsletter"
+              id="newsletter"
+              type="checkbox"
+              onChange={handleNewsletter}
+              checked={isNewsletter}
+            />
           </div>
-          <Button classes='button_center' label='wyślij' type='submit' />
+          <Button classes="button_center" label="wyślij" type="submit" />
         </form>
       </div>
     </section>
