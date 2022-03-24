0c99c3da88def4e010f6ac1f9335be11c8dcfeb9
krystek77 <krystian.wrona77@gmail.com>
2022-03-24 14:11:53 +0100

    Add contact data information

--- components/ContactItem/ContactItem.js
+++ components/ContactItem/ContactItem.js
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import styles from './ContactItem.module.css';
+import { classesNames } from '../../lib';
 
-function ContactItem({ to, src, alt, width, height, label }) {
+function ContactItem({ to, src, alt, width, height, label, classes = '' }) {
+  const classesString = classesNames(classes, 'contactItem')(styles);
   return (
-    <a href={to} className={styles.contactItem}>
+    <a href={to} className={classesString}>
       <Image
         src={`/assets/icons/${src}.svg`}
         alt={alt}
