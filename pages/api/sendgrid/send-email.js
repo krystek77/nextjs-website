f405dc36b154643c76e56b4b3a9c03f600a236a8
krystek77 <krystian.wrona77@gmail.com>
2022-03-24 20:53:03 +0100

    Send email by sendgrid

--- pages/api/sendgrid/send-email.js
+++ pages/api/sendgrid/send-email.js
@@ -1,40 +1,90 @@
-
-import sendGrid from "@sendgrid/mail";
-sendGrid.setApiKey = process.env.SEND_GRID_API_KEY;
-console.log(process.env.SEND_GRID_API_KEY);
+import sendGrid from '@sendgrid/mail';
+sendGrid.setApiKey(process.env.SEND_GRID_API_KEY);
 
 /**
  * endpoint: http://localhost:3000/api/sendgrid/send-email
  */
 
-const exampleData = {
-  reason: "gwarancja",
-  market: "stadniny koni, schroniska dla zwierząt",
-  name: "Zdzisław Wrona",
-  lastName: "Wro",
-  phone: "+48413450561",
-  city: "Kielce",
-  company: "PPUP Pralma sp. z o.o.",
-  email: "biuro@pralma.pl",
-  message: "Proin eget tortor risus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Nulla quis lorem ut libero malesuada feugiat.",
-};
-
 export default async function sendEmail(req, res) {
-  //   const { body: data } = req;
+  const { body: data } = req;
+  const HTML = `<html lang="pl">
+  <head>
+    <meta charset="utf-8"/>
+    <title>${data.reason}</title>
+  </head>
+  <body>
+  <header style="margin-bottom:24px;">
+      <h1 style="color:#001a23;margin:0px; font-size:22px;"><span style="font-weight:normal;font-size:14px;text-transform:uppercase;">Dotyczy:</span> ${data.reason}</h1>
+      <p style="font-size:22px;margin:0px;padding:0px"><span style="font-weight:normal;font-size:14px;text-transform:uppercase;">Sektor:</span> <b>${data.market}<b/></p>
+  </header>
+  <main">
+    <h2 style="color:#001a23;margin:0px;">Dane nadawcy wiadomości</h2>
+    <hr style="color:#001a23"/>
+    <p style="margin:0px;"><span style="font-weight:400;">Imię: </span><b>${data.name}</b></p>
+    <p style="margin:0px;"><span style="font-weight:400;">Nazwisko: </span><b>${data.lastName}</b></p>
+    <p style="margin:0px;"><span style="font-weight:400;">Telefon kontaktowy: </span><b>${data.phone}</b></p>
+    <p style="margin:0px;"><span style="font-weight:400;">Email: </span><a href="mailto:${data.email}"><b>${data.email}</b></a></p>
+    <h2 style="color:#001a23;margin:0px;">Treść wiadomości</h2>
+    <hr style="color:#001a23"/>
+    <p style="font-size:18px;line=height:1.3;background-color:#999999;margin:0px;padding-top:16px;padding-bottom:16px;padding-left:32px;padding-right:32px;">${data.message}</p>
+    <hr style="color:#001a23"/>
+    <p>Firma / instytucja: <b>${data.company}</b>, ${data.city}</p>
+    <span>Z poważaniem, </span>
+    <div><em>${data.name}</em><em> ${data.lastName}</em></div>
+  </main>
+  </body>
+  </html>`;
+  const TEXT = `Nowa wiadomość: ${data.reason}
+  Dotyczy: ${data.reason}
+  Sektor: ${data.market}
+  Dane nadawcy wiadomości
+  Imię: ${data.name}
+  Nazwisko: ${data.lastName}
+  Telefon kontaktowy: ${data.phone}
+  Email: ${data.email}
+
+  Treść wiadomości
+
+  ${data.message}
+  Firma / instytucja: ${data.company}, ${data.city}
+
+  Z poważaniem,
+  ${data.name} ${data.lastName}
+`;
+
   const message = {
-    to: "k.wrona@pralma.pl",
-    from: "k.wrona@pralma.pl",
-    subject: exampleData.reason,
-    text: `SOME TEXT from :${exampleData.email}`,
-    html: `<h1></h1>Masz nową wiadomość od: ${exampleData.name}</h1>
-    <p>${exampleData.message}</p>
-    `,
+    to: 'k.wrona@pralma.pl',
+    cc: {
+      email: 'krystianwrona.kielce@gmail.com',
+      name: 'Adresat kopii wiadomości',
+    },
+    bcc: {
+      email: 'krystian.wrona77@gmail.com',
+      name: 'Ukryty odbiorca kopii',
+    },
+    from: { email: 'k.wrona@pralma.pl', name: 'Ze strony PPUP Pralma' },
+    subject: `TEMAT: ${data.reason}`,
+    text: TEXT,
+    html: HTML,
   };
   try {
     await sendGrid.send(message);
-    return res.status(200).json({ message: "Wiadomość wysłana pomyślnie" });
+    return res.status(200).json({ message: 'Wiadomość wysłana pomyślnie' });
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    if (error.code) {
+      if (error.code === 403) {
+        return res.status(403).json({
+          message:
+            'Wyslano już 100 maili tego dnia. Plan nie pozwala wysłać już dzisiaj wiadomości',
+        });
+      }
+      if (error.code === 400) {
+        return res.status(400).json({ message: 'Niepoprawne żądanie' });
+      }
+      return res.status(401).json({
+        message: `Wysłanie wiadomości nie powiodło się`,
+      });
+    }
   }
-  return res.json(exampleData);
 }
