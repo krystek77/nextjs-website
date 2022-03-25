09b19c5deb981c13b8c92db07e6124be94afbfcb
krystek77 <krystian.wrona77@gmail.com>
2022-03-25 18:35:07 +0100

    Subscribe to newsletter

--- containers/Newsletter/Newsletter.js
+++ containers/Newsletter/Newsletter.js
@@ -3,15 +3,40 @@ import Title from "../../components/Title/Title";
 import Subtitle from "../../components/Subtitle/Subtitle";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import InputError from "../../components/InputError/InputError";
 
 import styles from "./Newsletter.module.css";
 
+const API_ROUTE_NEWSLETTER = "/api/sendgrid/newsletter";
+
 function Newsletter() {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
-  const subscribeToNewsletter = (e) => {
+  const isEmail = () => {
+    let isValid = true;
+    let error = false;
+    if (email.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/) === null) {
+      isValid = false;
+      error = true;
+    }
+    setError(error);
+    return isValid;
+  };
+
+  const subscribeToNewsletter = async (e) => {
     e.preventDefault();
-    clearInput();
+
+    if (isEmail()) {
+      try {
+        const response = await fetch(API_ROUTE_NEWSLETTER, { method: "PUT", headers: { "Content-Type": "application/json" }, body: JSON.stringify(email) });
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+      clearInput();
+    }
   };
   const handleInput = (e) => setEmail(e.target.value);
   const clearInput = () => setEmail("");
@@ -22,6 +47,7 @@ function Newsletter() {
         <Subtitle content='Bądż na bieżąco - wszystko co chcesz wiedzieć mając pralnię przemysłową' classes='subtitle_max_width_640' />
         <Title content='Newsletter' />
         <form onSubmit={subscribeToNewsletter} className={styles.newsletter__form}>
+          <InputError isError={error} message='Nieprawidłowy adres email' />
           <Input type='text' value={email} handleInput={handleInput} fieldName='email' placeholder='Wpisz swój email' />
           <Button type='submit' label='Zapisz się' />
         </form>
---
+++ pages/api/sendgrid/newsletter.js
@@ -0,0 +1,37 @@
+import client from "@sendgrid/client";
+client.setApiKey(process.env.SEND_GRID_API_KEY);
+
+export default async function newsletter(req, res) {
+  const { body: email } = req;
+  const data = {
+    contacts: [
+      {
+        email: email,
+      },
+    ],
+  };
+  const request = {
+    url: `/v3/marketing/contacts`,
+    method: "PUT",
+    body: data,
+  };
+
+  try {
+    const response = await client.request(request);
+    return res.status(201).json({ response });
+  } catch (error) {
+    if (error.code) {
+      if (error.code === 403) {
+        return res.status(403).json({
+          message: "Odmowa :(",
+        });
+      }
+      if (error.code === 400) {
+        return res.status(400).json({ message: "Niepoprawne żądanie" });
+      }
+      return res.status(401).json({
+        message: `Zapisanie na newsletter nie powiodło się`,
+      });
+    }
+  }
+}
