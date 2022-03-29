0a8454891ed68ab6ad2a81848bd1f098418fefd9
krystek77 <krystian.wrona77@gmail.com>
2022-03-29 18:29:38 +0200

    Create base structure of the form to add our laundry

--- pages/admin/nasze-pralnie/index.js
+++ pages/admin/nasze-pralnie/index.js
@@ -1,131 +1,90 @@
-import React from 'react';
-import AdminLayout from '../../../components/Layout/AdminLayout';
-import Title from '../../../components/Title/Title';
-import Input from '../../../components/Input/Input';
-import SelectInput from '../../../components/Select/Select';
-import Button from '../../../components/Button/Button';
-import styles from './index.module.css';
+import React from "react";
+import AdminLayout from "../../../components/Layout/AdminLayout";
+import Title from "../../../components/Title/Title";
+import Input from "../../../components/Input/Input";
+import SelectInput from "../../../components/Select/Select";
+import Button from "../../../components/Button/Button";
+import styles from "./index.module.css";
 
-const product = ['', '', ''];
-const models = ['', '', ''];
+const products = ["pralnicowirówka", "suszarka bębnwoa", "wózek", "prasownica nieckowa", "prasownica walcowa"];
+const models = ["FX-150", "FX-180", "FX-135", "FX-280", "FX-240"];
 
 function OurLaundries() {
+  const selectedFile = React.useRef(null);
   const [formData, setFormData] = React.useState({
-    title: '',
-    desc: '',
-    date: Date.now(),
-    from: '',
-    to: '',
-    product: '',
-    model: '',
-    amount: '',
-    image: '',
+    title: "",
+    desc: "",
+    from: "",
+    to: "",
+    product: products[0],
+    model: models[0],
+    amount: 1,
+    equipments: [],
   });
   const handleForm = (e) => {
     e.preventDefault();
+    const data = {
+      title: formData.title,
+      desc: formData.desc,
+      createdAt: new Date().toLocaleDateString("pl-PL"),
+      realization: {
+        from: formData.from,
+        to: formData.to,
+      },
+      equipments: formData.equipments,
+      image: selectedFile.current.files[0]?.name,
+    };
+    console.log(data);
   };
-
-  React.useEffect(() => {
-    console.log(formData);
-  });
+  const resetForm = () => {
+    console.log("RESET FORM");
+    setFormData({
+      title: "",
+      desc: "",
+      from: "",
+      to: "",
+      product: products[0],
+      model: models[0],
+      amount: 1,
+      equipments: [],
+    });
+    selectedFile.current.value = "";
+  };
+  const handleEquipments = () => {
+    const equipment = {
+      product: formData.product,
+      model: formData.model,
+      amount: Number(formData.amount),
+    };
+    const equipments = [...formData.equipments, equipment];
+    console.log(equipments);
+    setFormData({ ...formData, equipments: equipments });
+  };
 
   return (
     <div className={styles.ourLaundries}>
-      <Title content="Dodaj pralnię" classes="title_display_h5" variant="h2" />
+      <Title content='Dodaj pralnię' classes='title_display_h5' variant='h2' />
       <form className={styles.ourLaundries__form} onSubmit={handleForm}>
-        <Input
-          type="text"
-          fieldName="title"
-          placeholder="wpisz odbiorcę"
-          value={formData.title}
-          handleInput={(e) =>
-            setFormData({ ...formData, title: e.target.value })
-          }
-          classes="input_mb_1"
-        />
-        <textarea
-          onChange={(e) =>
-            setFormData({
-              ...formData,
-              desc: e.target.value,
-            })
-          }
-          className={styles.ourLaundries__description}
-          value={formData.desc}
-          name="desc"
-          rows="10"
-          placeholder="Krótki opis wyposażenie"
-        />
-        <Input
-          type="file"
-          fieldName="image"
-          placeholder="upload image"
-          handleInput={(e) =>
-            setFormData({ ...formData, image: e.target.files[0].name })
-          }
-          classes="input_mb_1"
-        />
-        <div>
-          <Input
-            type="date"
-            fieldName="from"
-            value={formData.from}
-            handleInput={(e) =>
-              setFormData({
-                ...formData,
-                from: e.target.value,
-              })
-            }
-            classes="input_mb_1"
-          />
-          <Input
-            type="date"
-            fieldName="to"
-            value={formData.to}
-            handleInput={(e) =>
-              setFormData({
-                ...formData,
-                to: e.target.value,
-              })
-            }
-            classes="input_mb_1"
-          />
+        <Input type='text' fieldName='title' placeholder='wpisz odbiorcę' value={formData.title} handleInput={(e) => setFormData({ ...formData, title: e.target.value })} classes='input_mb_1' />
+        <textarea onChange={(e) => setFormData({ ...formData, desc: e.target.value })} className={styles.ourLaundries__description} value={formData.desc} name='desc' rows='10' placeholder='Krótki opis wyposażenie' />
+        <input type='file' ref={selectedFile} />
+        <div className={styles.ourLaundries__dates}>
+          <Input type='date' fieldName='from' value={formData.from} handleInput={(e) => setFormData({ ...formData, from: e.target.value })} classes='input_mb_1' />
+          <Input type='date' fieldName='to' value={formData.to} handleInput={(e) => setFormData({ ...formData, to: e.target.value })} classes='input_mb_1' />
         </div>
-        <SelectInput
-          options={[
-            'pralnicowirówka',
-            'suszarka bębnwoa',
-            'wózek',
-            'prasownica nieckowa',
-            'prasownica walcowa',
-          ]}
-          action={(e) => setFormData({ ...formData, product: e.target.value })}
-          name="product"
-          selected={formData.product}
-        />
-        <SelectInput
-          options={['FX-150', 'FX-180', 'FX-135', 'FX-280', 'FX-240']}
-          action={(e) => setFormData({ ...formData, model: e.target.value })}
-          name="model"
-          selected={formData.model}
-        />
-        <Input
-          type="number"
-          fieldName="amount"
-          value={formData.amount}
-          placeholder="ilość"
-          handleInput={(e) =>
-            setFormData({ ...formData, amount: e.target.value })
-          }
-          classes="input_mb_1"
-        />
+        {/** equipments */}
+        <div className={styles.ourLaundries__equipmentsSection}>
+          <SelectInput options={products} action={(e) => setFormData({ ...formData, product: e.target.value })} name='product' selected={formData.product} />
+          <SelectInput options={models} action={(e) => setFormData({ ...formData, model: e.target.value })} name='model' selected={formData.model} />
+          <Input type='number' fieldName='amount' value={formData.amount} placeholder='ilość' handleInput={(e) => setFormData({ ...formData, amount: e.target.value })} classes='input_mb_1' />
+          <Button label='dodaj wyposażenie' classes='button_small button_no_wrap button_mb_1 button_to_right' action={handleEquipments}>
+            +
+          </Button>
+        </div>
+        {/** equipments */}
         <div className={styles.contactFormSection__actionButtons}>
-          <Button
-            classes="button_mr_2 button_no_wrap"
-            label="dodaj"
-            type="submit"
-          />
-          <Button classes="button_dangerous" label="zresetuj" type="button" />
+          <Button classes='button_mr_2 button_no_wrap' label='dodaj' type='submit' />
+          <Button classes='button_dangerous' label='zresetuj' type='button' action={resetForm} />
         </div>
       </form>
     </div>
