977d45bf95b4b0341017949447f89fd09c92a43c
krystek77 <krystian.wrona77@gmail.com>
2022-03-29 13:03:04 +0200

    Place a linkt to the admin dashboard page in the toolbar

--- containers/Topbar/Topbar.js
+++ containers/Topbar/Topbar.js
@@ -1,14 +1,22 @@
-import MainNavigation from "../../components/MainNavigation/MainNavigation";
-import SmallScreenNavigation from '../../components/SmallScreenNavigation/SmallScreenNavigation'
+import MainNavigation from '../../components/MainNavigation/MainNavigation';
+import SmallScreenNavigation from '../../components/SmallScreenNavigation/SmallScreenNavigation';
 import Logo from '../../components/Logo/Logo';
-import styles from "./Topbar.module.css";
+import LinkButton from '../../components/LinkButton/LinkButton';
+import styles from './Topbar.module.css';
 
 function Topbar() {
   return (
     <div className={styles.topbar}>
-      <Logo width="96" height="40"/>
+      <Logo width="96" height="40" />
       <MainNavigation />
-      <SmallScreenNavigation/>
+      {process.env.NODE_ENV !== 'production' && (
+        <LinkButton
+          to="/admin"
+          label="Moja Pralma"
+          classes="linkButton_small_text"
+        ></LinkButton>
+      )}
+      <SmallScreenNavigation />
     </div>
   );
 }
