2c3f11316af25699646264e5de0836e28af051f9
krystek77 <krystian.wrona77@gmail.com>
2022-03-30 08:09:58 +0200

    Add some titles to laundries form

--- pages/admin/nasze-pralnie/index.js
+++ pages/admin/nasze-pralnie/index.js
@@ -1,22 +1,28 @@
-import React from "react";
-import AdminLayout from "../../../components/Layout/AdminLayout";
-import Title from "../../../components/Title/Title";
-import Input from "../../../components/Input/Input";
-import SelectInput from "../../../components/Select/Select";
-import Button from "../../../components/Button/Button";
-import styles from "./index.module.css";
+import React from 'react';
+import AdminLayout from '../../../components/Layout/AdminLayout';
+import Title from '../../../components/Title/Title';
+import Input from '../../../components/Input/Input';
+import SelectInput from '../../../components/Select/Select';
+import Button from '../../../components/Button/Button';
+import styles from './index.module.css';
 
-const products = ["pralnicowirówka", "suszarka bębnwoa", "wózek", "prasownica nieckowa", "prasownica walcowa"];
-const models = ["FX-150", "FX-180", "FX-135", "FX-280", "FX-240"];
+const products = [
+  'pralnicowirówka',
+  'suszarka bębnwoa',
+  'wózek',
+  'prasownica nieckowa',
+  'prasownica walcowa',
+];
+const models = ['FX-150', 'FX-180', 'FX-135', 'FX-280', 'FX-240'];
 
 function OurLaundries() {
   const selectedFile = React.useRef(null);
   const [equipmentAdded, setEquipmentAdded] = React.useState(false);
   const [formData, setFormData] = React.useState({
-    title: "",
-    desc: "",
-    from: "",
-    to: "",
+    title: '',
+    desc: '',
+    from: '',
+    to: '',
     product: products[0],
     model: models[0],
     amount: 1,
@@ -27,7 +33,7 @@ function OurLaundries() {
     const data = {
       title: formData.title,
       desc: formData.desc,
-      createdAt: new Date().toLocaleDateString("pl-PL"),
+      createdAt: new Date().toLocaleDateString('pl-PL'),
       realization: {
         from: formData.from,
         to: formData.to,
@@ -38,18 +44,18 @@ function OurLaundries() {
     console.log(data);
   };
   const resetForm = () => {
-    console.log("RESET FORM");
+    console.log('RESET FORM');
     setFormData({
-      title: "",
-      desc: "",
-      from: "",
-      to: "",
+      title: '',
+      desc: '',
+      from: '',
+      to: '',
       product: products[0],
       model: models[0],
       amount: 1,
       equipments: [],
     });
-    selectedFile.current.value = "";
+    selectedFile.current.value = '';
   };
   const handleEquipments = () => {
     const equipment = {
@@ -62,7 +68,13 @@ function OurLaundries() {
     setFormData({ ...formData, equipments: equipments });
     setEquipmentAdded(true);
     //clear equipments
-    setFormData({ ...formData, equipments: [...equipments], product: products[0], model: models[0], amount: 1 });
+    setFormData({
+      ...formData,
+      equipments: [...equipments],
+      product: products[0],
+      model: models[0],
+      amount: 1,
+    });
   };
   React.useEffect(() => {
     const timer = setTimeout(function () {
@@ -75,29 +87,121 @@ function OurLaundries() {
 
   return (
     <div className={styles.ourLaundries}>
-      <Title content='Dodaj pralnię' classes='title_display_h5' variant='h2' />
+      <Title
+        content="Dodaj zrealizowane wyposażenie pralni"
+        classes="title_display_h5 "
+        variant="h2"
+      />
       <form className={styles.ourLaundries__form} onSubmit={handleForm}>
-        <Input type='text' fieldName='title' placeholder='wpisz odbiorcę' value={formData.title} handleInput={(e) => setFormData({ ...formData, title: e.target.value })} classes='input_mb_1' />
-        <textarea onChange={(e) => setFormData({ ...formData, desc: e.target.value })} className={styles.ourLaundries__description} value={formData.desc} name='desc' rows='10' placeholder='Krótki opis wyposażenie' />
-        <input type='file' ref={selectedFile} />
+        <Input
+          type="text"
+          fieldName="title"
+          placeholder="wpisz odbiorcę wyposażenia np.: Dom Pomocy Społecznej w Piłce Zamyślin"
+          value={formData.title}
+          handleInput={(e) =>
+            setFormData({ ...formData, title: e.target.value })
+          }
+          classes="input_mb_1"
+        />
+        {/** potential component */}
+        <textarea
+          className={styles.ourLaundries__description}
+          onChange={(e) => setFormData({ ...formData, desc: e.target.value })}
+          value={formData.desc}
+          name="desc"
+          rows="10"
+          placeholder="Krótki opis dostarczonego wyposażenia"
+        />
+        {/** end potential component */}
+        {/** potential component */}
+        <input
+          className={styles.ourLaundries__fileInput}
+          type="file"
+          ref={selectedFile}
+        />
+        {/** end potential component */}
+        <Title
+          content="Okres dostaw"
+          variant="h3"
+          classes="title_display_h6 title_uppercase"
+        />
         <div className={styles.ourLaundries__dates}>
-          <Input type='date' fieldName='from' value={formData.from} handleInput={(e) => setFormData({ ...formData, from: e.target.value })} classes='input_mb_1' />
-          <Input type='date' fieldName='to' value={formData.to} handleInput={(e) => setFormData({ ...formData, to: e.target.value })} classes='input_mb_1' />
+          <Input
+            type="date"
+            fieldName="from"
+            value={formData.from}
+            handleInput={(e) =>
+              setFormData({ ...formData, from: e.target.value })
+            }
+            classes="input_mb_1"
+          />
+          <Input
+            type="date"
+            fieldName="to"
+            value={formData.to}
+            handleInput={(e) =>
+              setFormData({ ...formData, to: e.target.value })
+            }
+            classes="input_mb_1"
+          />
         </div>
         {/** equipments */}
+        <Title
+          content="Wyposażenie"
+          variant="h3"
+          classes="title_display_h6 title_uppercase"
+        />
         <div className={styles.ourLaundries__equipmentsSection}>
-          <SelectInput options={products} action={(e) => setFormData({ ...formData, product: e.target.value })} name='product' selected={formData.product} />
-          <SelectInput options={models} action={(e) => setFormData({ ...formData, model: e.target.value })} name='model' selected={formData.model} />
-          <Input type='number' fieldName='amount' value={formData.amount} placeholder='ilość' handleInput={(e) => setFormData({ ...formData, amount: e.target.value })} classes='input_mb_1' />
-          <Button label='dodaj wyposażenie' classes='button_small button_no_wrap button_mb_1 button_to_right' action={handleEquipments}>
+          <SelectInput
+            options={products}
+            action={(e) =>
+              setFormData({ ...formData, product: e.target.value })
+            }
+            name="product"
+            selected={formData.product}
+          />
+          <SelectInput
+            options={models}
+            action={(e) => setFormData({ ...formData, model: e.target.value })}
+            name="model"
+            selected={formData.model}
+          />
+          <Input
+            type="number"
+            fieldName="amount"
+            value={formData.amount}
+            placeholder="ilość"
+            handleInput={(e) =>
+              setFormData({ ...formData, amount: e.target.value })
+            }
+            classes="input_mb_1"
+          />
+          <Button
+            label="dodaj wyposażenie"
+            classes="button_small button_no_wrap button_mb_1 button_to_right"
+            action={handleEquipments}
+          >
             +
           </Button>
-          {equipmentAdded && <span className={styles.ourLaundries_equipmentAddedIndicator}>Wyposażenie dodane</span>}
+          {equipmentAdded && (
+            <span className={styles.ourLaundries_equipmentAddedIndicator}>
+              Wyposażenie dodane
+            </span>
+          )}
         </div>
         {/** equipments */}
         <div className={styles.contactFormSection__actionButtons}>
-          <Button classes='button_mr_2 button_no_wrap' label='dodaj' type='submit' />
-          <Button classes='button_dangerous' label='zresetuj' type='button' action={resetForm} />
+          <Button
+            classes="button_mr_2 button_no_wrap"
+            label="dodaj"
+            type="submit"
+          />
+          <Button
+            classes="button_dangerous"
+            label="zresetuj"
+            type="button"
+            action={resetForm}
+          />
         </div>
       </form>
     </div>
