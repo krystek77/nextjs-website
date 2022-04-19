00b5cf00fcf94eb1aab75a85b78c37ebb3300b2b
krystek77 <krystian.wrona77@gmail.com>
2022-04-19 20:02:11 +0200

    Set backdround of the green passport banner

--- pages/ekologiczne-wyposazenie-pralni-przemyslowej.js
+++ pages/ekologiczne-wyposazenie-pralni-przemyslowej.js
@@ -1,12 +1,23 @@
 /*
  * http://localhost:3000/ekologiczne-wyposazenie-pralni-przemyslowej
  */
-
-import SiteInProgress from '../containers/SiteInProgress/SiteInProgress';
-import PageLayout from '../components/Layout/PageLayout';
+import React from "react";
+import Banner from "components/Banner/Banner";
+import Title from "components/Title/Title";
+import PageLayout from "../components/Layout/PageLayout";
+import SiteInProgress from "../containers/SiteInProgress/SiteInProgress";
+import Description from "components/Description/Description";
 
 function GreenPassport() {
-  return <SiteInProgress page="GREEN PASSPORT" />;
+  return (
+    <React.Fragment>
+      <Banner classes='banner__green_passport banner_maxWidth_760'>
+        <Title classes="title_center title_light_500" content='Sposób na ekologiczną i ekonomiczną pralnię wodną' />
+        <Description classes='description_center description_maxWidth_auto' content='Ponieważ zasoby stają się coraz bardziej cenne, rozwijamy rozwiązania pomagające Ci prać taniej i chronić przy tym naturalne zasoby środkowiska' />
+      </Banner>
+      <SiteInProgress />
+    </React.Fragment>
+  );
 }
 GreenPassport.getLayout = (page) => {
   return <PageLayout>{page}</PageLayout>;
