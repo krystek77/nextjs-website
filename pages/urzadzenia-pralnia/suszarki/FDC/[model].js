6cb8725245bd1b43c6190fbe9a801585d6fcd7b8
krystek77 <krystian.wrona77@gmail.com>
2022-04-25 14:11:47 +0200

    Start adding content for dryers

---
+++ pages/urzadzenia-pralnia/suszarki/FDC/[model].js
@@ -0,0 +1,105 @@
+import React from 'react';
+import HeadMetaTags from 'components/HeadMetaTags/HeadMetaTags';
+import Banner from 'components/Banner/Banner';
+import Title from 'components/Title/Title';
+import PageIndicator from 'components/Banner/PageIndicator/PageIndicator';
+import ProductInfo from 'containers/ProductInfo/ProductInfo';
+import IconLink from 'components/IconLink/IconLink';
+import ProductData from 'containers/ProductData/ProductData';
+import ProductControls from 'containers/ProductControls/ProductControls';
+import Leaflets from 'containers/Leaflets/Leaflets';
+import PageLayout from 'components/Layout/PageLayout';
+import { cutURL } from 'lib';
+import { useRouter } from 'next/router';
+import { sort } from 'utils/sortAvailableModels';
+import { model, modelsPaths } from 'utils/models';
+
+function FDC({ item }) {
+  const router = useRouter();
+  const {
+    model,
+    title,
+    category,
+    description,
+    line,
+    available_models,
+    slider,
+    isVertical,
+    features,
+    parameters,
+    controls,
+    leaflets,
+  } = item;
+  return (
+    <React.Fragment>
+      <HeadMetaTags
+        title={title}
+        description={description}
+        twitter_title={title}
+      />
+      <Banner classes="banner__dryer_T banner_height_auto">
+        <Title variant="h1" content={title} classes="title_bg_white_primary" />
+        <PageIndicator label={category} variant="primary" />
+      </Banner>
+      <main>
+        <ProductInfo
+          model={model}
+          line={line}
+          category={category}
+          available_models={sort(available_models)}
+          slider={slider}
+          vertical={isVertical}
+          description={description}
+        />
+        <IconLink
+          to={cutURL(router, -2)}
+          src="arrow_back"
+          alt="arrow back"
+          width="16"
+          height="11"
+          label="wróć"
+        />
+        <ProductData features={features} parameters={parameters} />
+        <ProductControls controls={controls} />
+        <Leaflets leaflets={leaflets} />
+      </main>
+    </React.Fragment>
+  );
+}
+FDC.getLayout = (page) => {
+  return <PageLayout>{page}</PageLayout>;
+};
+FDC.defaultProps = {
+  model: 'FDC',
+  title: 'Some awesome title',
+  category: 'Category name',
+  description: 'Some description',
+  line: 'Line name',
+  available_models: [],
+  slider: [],
+  isVertical: false,
+  features: [],
+  parameters: [],
+  controls: [],
+  leaflets: [],
+};
+export default FDC;
+
+const TYPE_NAME = 'FDC';
+const CATEGORY = 'suszarki';
+export async function getStaticPaths() {
+  const paths = await modelsPaths(CATEGORY, TYPE_NAME);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps(context) {
+  const oneModel = await model(CATEGORY, context, TYPE_NAME);
+
+  return {
+    props: {
+      item: JSON.parse(JSON.stringify(oneModel)),
+    },
+  };
+}
